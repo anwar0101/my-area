808183fb45fc366bc8e5921e18504c6e48a6cb98
Anwar Hussen <anwar.hussen4@gmail.com>
2017-05-05 21:45:30 +0600

    My Area app with TransitionService

---
+++ app/src/main/java/com/blackholetechbd/myarea/GeofenceRegistrationService.java
@@ -0,0 +1,40 @@
+package com.blackholetechbd.myarea;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+public class GeofenceRegistrationService extends IntentService {
+
+    private static final String TAG = "GeoIntentService";
+
+    public GeofenceRegistrationService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            Log.d(TAG, "GeofencingEvent error " + geofencingEvent.getErrorCode());
+        } else {
+            int transaction = geofencingEvent.getGeofenceTransition();
+            List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
+            Geofence geofence = geofences.get(0);
+            if (transaction == Geofence.GEOFENCE_TRANSITION_ENTER && geofence.getRequestId().equals(Constants.GEOFENCE_ID_STAN_UNI)) {
+                Log.d(TAG, "You are inside Stanford University");
+                Toast.makeText(this, "You are inside Stanford University", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d(TAG, "You are outside Stanford University");
+                Toast.makeText(this, "You are outside Stanford University", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
